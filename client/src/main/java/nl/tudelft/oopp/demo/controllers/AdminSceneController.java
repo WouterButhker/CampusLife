21ff0668d191e822dafc50a45437396759dc166b
HendyLiang <H.Liang-3@student.tudelft.nl>
2020-02-25 15:41:40 +0100

    This commit will add improved opening hours input for adding buildings and the buildings page will be able to display all the buildings in a list
    The buttons added in the display of the buildings aren't functional yet but should be able to respectively modify and delete buildings in the future

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/AdminSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/AdminSceneController.java
@@ -2,19 +2,23 @@ package nl.tudelft.oopp.demo.controllers;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
@@ -64,14 +68,22 @@ public class AdminSceneController implements Initializable {
     private TextField buildingCodeInput;
 
     @FXML
-    private TextField openingHoursInput;
+    private ChoiceBox<String> fromChoicebox;
 
     @FXML
-    private Text submitStatus;
+    private ChoiceBox<String> toChoicebox;
+
+    @FXML
+    private AnchorPane anchorPaneBuildings;
+
+    @FXML
+    private VBox vBoxBuildings;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         loadDataBikes();
+        loadOpeningHoursChoices();
+        loadBuildings();
     }
 
 
@@ -85,7 +97,56 @@ public class AdminSceneController implements Initializable {
                 }
             });
         }
+    }
 
+    private void loadOpeningHoursChoices() {
+        if (toChoicebox != null && fromChoicebox != null) {
+            String[] times = new String[25];
+            for (int i = 0; i < 25; i++) {
+                if (i < 10) {
+                    times[i] = "0" + i + ":00";
+                } else {
+                    times[i] = i + ":00";
+                }
+            }
+            toChoicebox.getItems().addAll(times);
+            fromChoicebox.getItems().addAll(times);
+        }
+    }
+
+    private void loadBuildings() {
+        if (anchorPaneBuildings != null && vBoxBuildings != null) {
+            int numBuildings = 10;
+            String[] buildings = ServerCommunication.getBuildingsCodeAndName();
+            anchorPaneBuildings.setPrefHeight(50 * numBuildings);
+            //One HBox is 50 x 300
+            //The entire AnchorPane holding all the HBoxes is 50*numBuildings x 300
+            for (int i = 0; i < numBuildings; i++) {
+                HBox hBox = new HBox();
+                hBox.setMaxWidth(300);
+                Image image = new Image("images/TuDelftTempIMG.jpg");
+                ImageView imageView = new ImageView(image);
+                imageView.setFitWidth(65);
+                imageView.setFitHeight(40);
+                Label text = new Label(buildings[i]);
+                text.setPrefSize(125, 40);
+                Button modify = new Button("modify");
+                modify.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+
+                    }
+                });
+                modify.setPrefSize(50, 40);
+                Button delete = new Button("delete");
+                delete.setPrefSize(50, 40);
+                hBox.setPadding(new Insets(5, 5, 5 ,5));
+                String css = "-fx-border-color: black;\n-fx-border-insets: 5\n;-fx-border-style: solid";
+                hBox.setStyle(css);
+                hBox.getChildren().addAll(imageView, text, modify, delete);
+                vBoxBuildings.getChildren().add(hBox);
+            }
+        }
     }
 
 
@@ -98,8 +159,9 @@ public class AdminSceneController implements Initializable {
         return bikes;
     }
 
-    public void addBike() {
-        if(bikeBuildings.getValue() != null) {
+    @FXML
+    private void addBike() {
+        if (bikeBuildings.getValue() != null) {
             int bikes = updateNumberBikes();
             bikes++;
             //send the new bikes to the backend
@@ -107,8 +169,9 @@ public class AdminSceneController implements Initializable {
         }
     }
 
-    public void removeBike() {
-        if(bikeBuildings.getValue() != null) {
+    @FXML
+    private void removeBike() {
+        if (bikeBuildings.getValue() != null) {
             int bikes = updateNumberBikes();
             bikes--;
             //send the new bikes to the backend
@@ -197,26 +260,32 @@ public class AdminSceneController implements Initializable {
     }
 
     @FXML
-    private void submitNewBuilding() throws IOException {
+    private void submitNewBuilding() {
         String location = locationInput.getText();
         String name = nameInput.getText();
         boolean codeFound = false;
         int buildingCode = 0;
         try {
-            buildingCode = Integer.parseInt(buildingCodeInput.getText());
+            buildingCode = Integer.parseInt(buildingCodeInput.getText().trim());
             codeFound = true;
         } catch (NumberFormatException e) {
-
+            e.printStackTrace();
         }
-        //String openingHours = openingHoursInput.getText();
-        String openingHours = "08:00-22:00";
-
+        String openingHours = fromChoicebox.getValue() + "-" + toChoicebox.getValue();
         Text submitStatus = new Text();
-        if (location != null && name != null && codeFound && openingHours != null) {
+        if (!location.equals("") && !name.equals("") && codeFound && fromChoicebox.getValue() != null && toChoicebox.getValue() != null) {
             ServerCommunication.addBuildingToDatabase(buildingCode, name, location, openingHours);
             submitStatus.setText("Building successfully added!");
         } else {
             submitStatus.setText("The input is wrong or not all fields are entered");
+        }
+
+        if (!codeFound) {
+            submitStatus.setText("The building code has to be a number!");
+        }
+
+        if ((fromChoicebox.getValue() != null && toChoicebox.getValue() != null) && fromChoicebox.getValue().compareTo(toChoicebox.getValue()) >= 0) {
+            submitStatus.setText("These opening hours don't make sense");
         }
 
         StackPane root = new StackPane(submitStatus);
@@ -228,4 +297,6 @@ public class AdminSceneController implements Initializable {
         stage.initOwner(modifyBuildingsExit.getScene().getWindow());
         stage.showAndWait();
     }
+
+
 }
