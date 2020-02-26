89909e10c91eb6a147edc8d972843edeafb906d8
Ana Baltaretu <A.Baltaretu@student.tudelft.nl>
2020-02-26 17:38:25 +0100

    Add Room and Building classes for the client side

---
+++ client/src/main/java/nl/tudelft/oopp/demo/entities/Building.java
@@ -0,0 +1,90 @@
+package nl.tudelft.oopp.demo.entities;
+
+import java.util.Objects;
+import javafx.scene.image.Image;
+
+public class Building {
+
+    private Integer code;
+    private String name;
+    private String location;
+    private String openingHours;
+    private Image image;
+
+    /**
+     * Creates a new Building object.
+     * @param code the number of the Building
+     * @param name a String with the full name of the Building
+     * @param location a String with the street
+     * @param openingHours a String with format hh:mm-hh:mm
+     * @param image a photo of the building
+     */
+    public Building(Integer code, String name, String location, String openingHours, Image image) {
+        this.code = code;
+        this.name = name;
+        this.location = location;
+        this.openingHours = openingHours;
+        this.image = image;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getOpeningHours() {
+        return openingHours;
+    }
+
+    public void setOpeningHours(String openingHours) {
+        this.openingHours = openingHours;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Building)) {
+            return false;
+        }
+        Building building = (Building) o;
+        return code.equals(building.code)
+                && name.equals(building.name)
+                && location.equals(building.location)
+                && openingHours.equals(building.openingHours)
+                && image.equals(building.image);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, name, location, openingHours, image);
+    }
+}
