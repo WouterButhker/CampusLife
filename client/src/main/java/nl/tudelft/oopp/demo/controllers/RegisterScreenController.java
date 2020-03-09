f44ee7a5889cbfd4bfe292714270e0889d20530c
krisstesink <K.A.Tesink@student.tudelft.nl>
2020-03-09 15:12:12 +0100

    Change format of register screen and add navigation

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/RegisterScreenController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/RegisterScreenController.java
@@ -8,9 +8,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import nl.tudelft.oopp.demo.communication.AuthenticationCommunication;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
 import nl.tudelft.oopp.demo.entities.UserDtO;
+import nl.tudelft.oopp.demo.widgets.AppBar;
 import org.springframework.boot.jackson.JsonObjectSerializer;
 import org.springframework.boot.json.JsonParser;
 import org.springframework.http.ResponseEntity;
@@ -26,25 +29,10 @@ public class RegisterScreenController {
     private URL location;
 
     @FXML
-    private Button okButtonPassword1;
-
-    @FXML
-    private Button okButtonPassword2;
-
-    @FXML
-    private Button okButtonUsername;
+    private Text errorMessage;
 
     @FXML
     private PasswordField passwordField;
-
-    @FXML
-    private Pane popupPassword1;
-
-    @FXML
-    private Pane popupPassword2;
-
-    @FXML
-    private Pane popupUsername;
 
     @FXML
     private PasswordField reEnterPasswordField;
@@ -56,31 +44,22 @@ public class RegisterScreenController {
     private TextField usernameField;
 
     @FXML
-    void onOkPassword1Clicked(ActionEvent event) {
-        popupPassword1.setVisible(false);
-        passwordField.setText("");
-        reEnterPasswordField.setText("");
-    }
+    private VBox mainBox;
+
+
 
     @FXML
-    void onOkPassword2Clicked(ActionEvent event) {
-        popupPassword2.setVisible(false);
-    }
-
-    @FXML
-    void onOkUsernameClicked(ActionEvent event) {
-        popupUsername.setVisible(false);
-    }
-
-    @FXML
-    void onRegisterClicked(ActionEvent event) {
-        if (usernameField.getText().equals("")) {
-            popupUsername.setVisible(true);
-        } else if (passwordField.getText().equals("")) {
-            popupPassword2.setVisible(true);
+    private void onRegisterClicked(ActionEvent event) {
+        if (usernameField.getText().equals("")
+                || passwordField.getText().equals("")
+                || reEnterPasswordField.getText().equals("")) {
+            errorMessage.setText("All fields must be filled in");
         } else if (!(passwordField.getText().equals(reEnterPasswordField.getText()))) {
-            popupPassword1.setVisible(true);
+            errorMessage.setText("Passwords do not match");
+            passwordField.setText("");
+            reEnterPasswordField.setText("");
         } else {
+            errorMessage.setText("");
             register(usernameField.getText(), passwordField.getText());
         }
     }
@@ -95,21 +74,13 @@ public class RegisterScreenController {
 
     }
 
+    private void addAppBar() {
+        mainBox.getChildren().add(0, new AppBar());
+    }
+
     @FXML
     void initialize() {
-        assert okButtonPassword1 != null : "fx:id=\"okButtonPassword1\" "
-                + "was not injected: check your FXML file 'RegisterScreen.fxml'.";
-        assert okButtonPassword2 != null : "fx:id=\"okButtonPassword2\" "
-                + "was not injected: check your FXML file 'RegisterScreen.fxml'.";
-        assert okButtonUsername != null : "fx:id=\"okButtonUsername\" "
-                + "was not injected: check your FXML file 'RegisterScreen.fxml'.";
         assert passwordField != null : "fx:id=\"passwordField\" "
-                + "was not injected: check your FXML file 'RegisterScreen.fxml'.";
-        assert popupPassword1 != null : "fx:id=\"popupPassword1\" "
-                + "was not injected: check your FXML file 'RegisterScreen.fxml'.";
-        assert popupPassword2 != null : "fx:id=\"popupPassword2\" "
-                + "was not injected: check your FXML file 'RegisterScreen.fxml'.";
-        assert popupUsername != null : "fx:id=\"popupUsername\" "
                 + "was not injected: check your FXML file 'RegisterScreen.fxml'.";
         assert reEnterPasswordField != null : "fx:id=\"reEnterPasswordField\" "
                 + "was not injected: check your FXML file 'RegisterScreen.fxml'.";
@@ -118,7 +89,7 @@ public class RegisterScreenController {
         assert usernameField != null : "fx:id=\"usernameField\" "
                 + "was not injected: check your FXML file 'RegisterScreen.fxml'.";
 
-
+        addAppBar();
     }
 
 
