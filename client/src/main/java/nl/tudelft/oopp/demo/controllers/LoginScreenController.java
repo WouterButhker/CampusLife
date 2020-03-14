08e14b756f736e9563f1e75cdb365d5c0a47b649
krisstesink <K.A.Tesink@student.tudelft.nl>
2020-03-14 17:33:14 +0100

    Fix checkstyle

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/LoginScreenController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/LoginScreenController.java
@@ -11,13 +11,10 @@ import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import nl.tudelft.oopp.demo.communication.AuthenticationCommunication;
-
-
 import nl.tudelft.oopp.demo.core.RoutingScene;
 import nl.tudelft.oopp.demo.core.XmlRoute;
 import nl.tudelft.oopp.demo.views.MainMenuRoute;
