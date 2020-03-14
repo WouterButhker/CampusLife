dcfc6f38b950e0699b465302917872e235a4adfa
Wouter Büthker <w.w.buthker@student.tudelft.nl>
2020-03-14 18:12:43 +0100

    Possible fix for pipeline

--- server/src/test/java/nl/tudelft/oopp/demo/AuthTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/AuthTest.java
@@ -1,5 +1,6 @@
 package nl.tudelft.oopp.demo;
 
+import nl.tudelft.oopp.demo.config.SecurityConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -18,7 +20,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @Transactional
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@ContextConfiguration( classes = {
+        DemoApplication.class,
+        SecurityConfiguration.class
+})
 public class AuthTest {
 
     @Autowired
