c5bd0747d6777bcd81ede63f9a223ed8da2be754
Wouter Büthker <w.w.buthker@student.tudelft.nl>
2020-03-14 18:01:09 +0100

    Revert change

--- server/src/test/java/nl/tudelft/oopp/demo/AuthTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/AuthTest.java
@@ -17,6 +17,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
+@Transactional
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AuthTest {
 
