5366b865aac7e0552a1cbfecebbdc91bc10e0129
Willem Stuijt <W.L.StuijtGiacaman@student.tudelft.nl>
2020-03-15 13:28:41 +0100

    Add authenticated POST and DELETE requests to client

--- client/src/main/java/nl/tudelft/oopp/demo/communication/ServerCommunication.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/ServerCommunication.java
@@ -7,10 +7,19 @@ public class ServerCommunication {
 
     public static final String SERVER_URL = "http://localhost:8080";
 
-
     public static ResponseEntity<String> authenticatedRequest(String link)
             throws AuthenticationException {
         return AuthenticationCommunication.authenticatedRequest(link);
+    }
+
+    public static ResponseEntity<String> authenticatedPostRequest(String link, Object body)
+            throws AuthenticationException {
+        return AuthenticationCommunication.authenticatedPostRequest(link, body);
+    }
+
+    public static ResponseEntity<String> authenticatedDeleteRequest(String link)
+            throws AuthenticationException {
+        return AuthenticationCommunication.authenticatedDeleteRequest(link);
     }
 
 }
