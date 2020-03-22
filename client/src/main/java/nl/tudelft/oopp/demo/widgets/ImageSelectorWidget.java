58c5bd21a559104eae1b0084eb0ad3c6f1bb43d0
krisstesink <K.A.Tesink@student.tudelft.nl>
2020-03-22 16:36:13 +0100

    Add more functionality to the widget and make it easier to use
    Now you can create an imageSelector widget and add it to any javafx scene easily

--- client/src/main/java/nl/tudelft/oopp/demo/widgets/ImageSelectorWidget.java
+++ client/src/main/java/nl/tudelft/oopp/demo/widgets/ImageSelectorWidget.java
@@ -1,12 +1,57 @@
 package nl.tudelft.oopp.demo.widgets;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 
 import java.io.File;
 
-public class ImageSelectorWidget {
+public class ImageSelectorWidget extends VBox {
+    private HBox chooseFile;
+    private Button chooseFileButton;
+    private Label fileChosen;
 
-    public static File getImage() {
+    private Button sendFile;
+
+    private File image;
+
+    /**
+     * Creates a new ImageSelectorWidget
+     */
+    public ImageSelectorWidget() {
+        Label selectImage = new Label("Select Image:");
+        selectImage.setStyle("-fx-font-weight: bold;");
+        this.fileChosen = new Label("  No file chosen");
+        this.chooseFileButton = new Button("Choose Image");
+        this.sendFile = new Button("Send");
+
+        this.chooseFile = new HBox();
+        this.chooseFile.setPrefHeight(32);
+        this.chooseFile.setAlignment(Pos.CENTER_LEFT);
+        this.chooseFile.getChildren().addAll(chooseFileButton, fileChosen);
+
+        this.getChildren().addAll(selectImage, chooseFile, sendFile);
+        this.setAlignment(Pos.CENTER_LEFT);
+
+        chooseFileButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                getImage();
+            }
+        });
+
+        sendFile.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                upload();
+            }
+        });
+    }
+
+    public void getImage() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Image Files", "*.jpg", "*.png"));
         File selectedFile = fileChooser.showOpenDialog(null);
@@ -14,11 +59,21 @@ public class ImageSelectorWidget {
         if (selectedFile != null) {
 
             System.out.println("File selected: " + selectedFile.getName());
+            this.fileChosen.setText("  " + selectedFile.getName());
         }
         else {
             System.out.println("File selection cancelled.");
         }
 
-        return selectedFile;
+        this.image = selectedFile;
+    }
+
+    public void upload() {
+        if(this.image != null) {
+            System.out.println(this.image.getName());
+        } else {
+            System.out.println("No file selected");
+        }
+
     }
 }
