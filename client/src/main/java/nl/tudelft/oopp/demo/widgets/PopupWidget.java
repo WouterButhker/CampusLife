3ff0a1b5e6be598b2fdbc495621d33e14bd81eae
krisstesink <K.A.Tesink@student.tudelft.nl>
2020-03-26 15:54:22 +0100

    Add method overloading to add a specified title for the popup

--- client/src/main/java/nl/tudelft/oopp/demo/widgets/PopupWidget.java
+++ client/src/main/java/nl/tudelft/oopp/demo/widgets/PopupWidget.java
@@ -33,4 +33,29 @@ public class PopupWidget {
         popupwindow.showAndWait();
     }
 
+    /**
+     * Creates a popup that displays the parameter message, and has a specified title.
+     * @param message The message that is to be displayed in the popup
+     * @param title The title of the popup
+     */
+    public static void display(String message, String title) {
+        Stage popupwindow = new Stage();
+        popupwindow.setTitle(title);
+        popupwindow.initModality(Modality.APPLICATION_MODAL);
+        Label label1 = new Label(message);
+        Button button1 = new Button(" OK ");
+
+        label1.setStyle("-fx-font-size: 14");
+        button1.setStyle("-fx-font-size: 14");
+
+        button1.setOnAction(e -> popupwindow.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label1, new Label(""), button1);
+        layout.setAlignment(Pos.CENTER);
+        Scene scene1 = new Scene(layout, 370, 190);
+        popupwindow.setScene(scene1);
+        popupwindow.showAndWait();
+    }
+
 }
