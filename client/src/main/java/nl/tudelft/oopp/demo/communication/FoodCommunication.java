3de40d0c720e2b9538e7fe2fe0087106a8c2229f
andradiana4231 <a.d.maglas@student.tudelft.nl>
2020-03-28 15:01:43 +0100

    Fix checkstyle

--- client/src/main/java/nl/tudelft/oopp/demo/communication/FoodCommunication.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/FoodCommunication.java
@@ -2,11 +2,10 @@ package nl.tudelft.oopp.demo.communication;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import nl.tudelft.oopp.demo.entities.Food;
-import nl.tudelft.oopp.demo.entities.Restaurant;
-
 import java.lang.reflect.Type;
 import java.util.List;
+import nl.tudelft.oopp.demo.entities.Food;
+import nl.tudelft.oopp.demo.entities.Restaurant;
 
 public class FoodCommunication {
 
