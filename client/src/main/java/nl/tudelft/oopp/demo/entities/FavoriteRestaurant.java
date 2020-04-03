91971c35ebfbbbbc75d9fc14185ee4e73cf5a180
Willem Stuijt <W.L.StuijtGiacaman@student.tudelft.nl>
2020-04-03 14:24:50 +0200

    Add FavoriteRestaurantCommunication

--- client/src/main/java/nl/tudelft/oopp/demo/entities/FavoriteRestaurant.java
+++ client/src/main/java/nl/tudelft/oopp/demo/entities/FavoriteRestaurant.java
@@ -3,16 +3,16 @@ package nl.tudelft.oopp.demo.entities;
 public class FavoriteRestaurant {
 
     private Integer id;
-    private Integer restaurant;
-    private Integer user;
+    private Restaurant restaurant;
+    private User user;
 
     /**
      * Makes a new FavoriteRestaurant object.
      * @param id the id of the favorite pair in the database
-     * @param restaurant the id of the restaurant
-     * @param user the id of the user
+     * @param restaurant the restaurant
+     * @param user the user
      */
-    public FavoriteRestaurant(Integer id, Integer restaurant, Integer user) {
+    public FavoriteRestaurant(Integer id, Restaurant restaurant, User user) {
         this.id = id;
         this.restaurant = restaurant;
         this.user = user;
@@ -26,19 +26,19 @@ public class FavoriteRestaurant {
         this.id = id;
     }
 
-    public Integer getRestaurant() {
+    public Restaurant getRestaurant() {
         return restaurant;
     }
 
-    public void setRestaurant(Integer restaurant) {
+    public void setRestaurant(Restaurant restaurant) {
         this.restaurant = restaurant;
     }
 
-    public Integer getUser() {
+    public User getUser() {
         return user;
     }
 
-    public void setUser(Integer user) {
+    public void setUser(User user) {
         this.user = user;
     }
 }
