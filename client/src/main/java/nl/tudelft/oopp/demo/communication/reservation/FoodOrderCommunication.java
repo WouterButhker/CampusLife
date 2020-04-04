6bb73a08a6deae807301b28e128a80671ecb1b04
Willem Stuijt <W.L.StuijtGiacaman@student.tudelft.nl>
2020-04-04 17:14:38 +0200

    Add getAll function to FoodOrderCommunication

--- client/src/main/java/nl/tudelft/oopp/demo/communication/reservation/FoodOrderCommunication.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/reservation/FoodOrderCommunication.java
@@ -1,8 +1,13 @@
 package nl.tudelft.oopp.demo.communication.reservation;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import nl.tudelft.oopp.demo.communication.AuthenticationCommunication;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
 import nl.tudelft.oopp.demo.entities.reservation.FoodOrder;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class FoodOrderCommunication {
 
@@ -16,6 +21,20 @@ public class FoodOrderCommunication {
                     "/foodOrder", foodOrder).getBody();
             Gson gson = new Gson();
             return gson.fromJson(responseString, FoodOrder.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static List<FoodOrder> getAll() {
+        try {
+            String responseString = ServerCommunication.authenticatedRequest(
+                    String.format("/foodOrder/user/%d", AuthenticationCommunication.myUserId)
+            ).getBody();
+            Gson gson = new Gson();
+            Type listType = new TypeToken<List<FoodOrder>>() {}.getType();
+            return gson.fromJson(responseString, listType);
         } catch (Exception e) {
             e.printStackTrace();
         }
