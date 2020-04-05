da85311333776bab2a2817c6521d6fe8f5e7c93b
Wouter Büthker <w.w.buthker@student.tudelft.nl>
2020-04-05 23:11:21 +0200

    Add Modifying tag to query

--- server/src/main/java/nl/tudelft/oopp/demo/repositories/UserRepository.java
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package nl.tudelft.oopp.demo.repositories;
 import java.util.Optional;
 import nl.tudelft.oopp.demo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 
@@ -18,6 +19,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("SELECT u.role FROM User u WHERE u.username = ?1")
     String findRoleByUsername(String username);
 
+    @Modifying
     @Query("UPDATE User u SET u.password = :username WHERE u.username = :password")
     int updatePassword(String username, String password);
 }
