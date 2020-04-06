0357314053b9544f9643ccc49d4ae67e79e862f1
Tsin Yu <T.Y.Huang@student.tudelft.nl>
2020-04-06 15:25:35 +0200

    Create favorite controller test files and delete duplicate bike reservation test

---
+++ server/src/test/java/nl/tudelft/oopp/demo/controllers/FavoriteRestaurantControllerTest.java
@@ -0,0 +1,29 @@
+package nl.tudelft.oopp.demo.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FavoriteRestaurantControllerTest {
+
+    @BeforeEach
+    void setUpper() {
+    }
+
+    @Test
+    void addFavoriteRestaurantTest() {
+    }
+
+    @Test
+    void getFavoriteRestaurantsTest() {
+    }
+
+    @Test
+    void deleteFavoriteRestaurantTest() {
+    }
+
+    @Test
+    void getFavoriteRestaurantTest() {
+    }
+}
---
+++ server/src/test/java/nl/tudelft/oopp/demo/controllers/FavoriteRoomControllerTest.java
@@ -0,0 +1,29 @@
+package nl.tudelft.oopp.demo.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FavoriteRoomControllerTest {
+
+    @BeforeEach
+    void setUpper() {
+    }
+
+    @Test
+    void addFavoriteRoomTest() {
+    }
+
+    @Test
+    void getFavoriteRoomsTest() {
+    }
+
+    @Test
+    void deleteFavoriteRoomTest() {
+    }
+
+    @Test
+    void getFavoriteRoomTest() {
+    }
+}
